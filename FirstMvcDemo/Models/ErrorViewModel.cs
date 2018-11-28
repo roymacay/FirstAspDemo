905eb1c35a914d8cbe1bab8bf9c7ef751a44a5fb
phuclehuu <phuc.le@tech.reapra.sg>
2018-11-28 14:21:31 +0700

    Upload code

---
+++ FirstMvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstMvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
